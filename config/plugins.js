86dedf53a7249c64925c5b7dfbf3900490c962bd
Sayed Akhtar(Mac M1) <sayed182@gmail.com>
2025-02-23 20:41:25 +0530

    added digitalocean spaces config for static assets

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,19 @@
-module.exports = () => ({
-'strapi-plugin-fcm': {
+module.exports = ({ env }) => ({
+  'strapi-plugin-fcm': {
     enabled: false,
     resolve: './node_modules/strapi-plugin-fcm' // path to plugin folder
   },
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: env("DO_SPACE_ACCESS_KEY"),  // DigitalOcean Access Key
+        secret: env("DO_SPACE_SECRET_KEY"), // DigitalOcean Secret Key
+        endpoint: env("DO_SPACE_ENDPOINT"), // DigitalOcean Endpoint (e.g., https://nyc3.digitaloceanspaces.com)
+        space: env("DO_SPACE_NAME"), // Your Space name
+        directory: env("DO_SPACE_FOLDER", ""), // Optional: Subfolder inside your Space
+        cdn: env("DO_SPACE_CDN", ""), // Optional: CDN URL
+      }
+    }
+  }
 });
