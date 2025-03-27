23ddd68a7100907584bf5e8be6b378c1d0169492
Sayed Akhtar(Mac M1) <sayed182@gmail.com>
2025-03-27 17:21:48 +0530

    path changed

--- src/api/user/routes/custom-login.js
+++ src/api/user/routes/custom-login.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             method: "POST",
             path: "/auth/login-mobile",
-            handler: "custom-login.login",
+            handler: "user.controllers.custom-login.login",
             config: {
                 auth: false, // No authentication required to access this route
             },
